312fa661a7380959fc4458e0e6035db051ad15bc
Abhishekkugali <126229384+Abhishekkugali@users.noreply.github.com>
2023-03-06 11:29:27 +0530

    Add files via upload

---
+++ App1.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./app1.css";
+
+
+const App1=()=>{
+    return(
+        <div id="ii">
+
+        <img src="https://1000logos.net/wp-content/uploads/2016/10/apple-emblem.jpg" alt="" />
+        <div className="menuBlock">
+            <ul>
+                <li>
+                    <a href="#jn">HOME</a>
+                </li>
+                <li>
+                    <a href="#gu">LOGIN</a>
+                </li>
+                <li>
+                    <a href="#jh">Sign-up</a>
+                </li>
+                <li>
+                    <a href="#yfy">ABOUT</a>
+                </li>
+            </ul>
+        </div>
+
+        </div>
+    )
+}
+export default App1;
